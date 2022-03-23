2c1cc54fde5f066a40b40457ba8798b6d055d475
infor2020 <lidiaalmeidaw@gmail.com>
2022-03-23 11:30:05 -0300

    Meu primeiro commit

---
+++ deletar.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once('config/conexao.php');
+if(isset($_GET['idDel'])){
+    $id = $_GET['idDel'];
+    echo $id;
+    //echo $id;
+    $deletar = "DELETE FROM tb_contato WHERE id_contato = :id";
+    try{
+        $result = $conect->prepare($deletar);
+        $result->bindParam(':id',$id,PDO::PARAM_INT);
+        $result->execute();
+
+        $contar = $result->rowCount();
+        if($contar>0){
+            header("Location: relatorio.php");
+        }else{
+
+        }
+
+    }catch(PDOException $e){
+        echo "<strong>ERRO AO DELETAR: </strong>".$e->getMessage();
+    }
+}
